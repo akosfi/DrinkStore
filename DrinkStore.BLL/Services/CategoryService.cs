f575cad5f4eb0be17d527178a53a13cb306a242c
Ákos Fi <fiakos1997@gmail.com>
2020-03-29 14:03:53 +0200

    Added Category Service

---
+++ DrinkStore.BLL/Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using DrinkStore.DAL;
+using DrinkStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkStore.BLL.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DrinkStoreContext _context;
+
+        public CategoryService(DrinkStoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context
+                .Categories
+                .Include(c => c.Subcategories)
+                .ToListAsync();
+        }
+    }
+}
---
+++ DrinkStore.BLL/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using DrinkStore.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkStore.BLL.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategories();
+    }
+}
