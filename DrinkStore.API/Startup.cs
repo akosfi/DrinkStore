b0af61a8441b5dc91c9fb9b65de8ea5b63988708
Ákos Fi <fiakos1997@gmail.com>
2020-03-31 12:47:46 +0200

    Allow CORS

--- DrinkStore.API/Startup.cs
+++ DrinkStore.API/Startup.cs
@@ -25,10 +25,24 @@ namespace DrinkStore.API
         }
 
         public IConfiguration Configuration { get; }
+        readonly string AllowedClientOrigin = nameof(AllowedClientOrigin);
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowedClientOrigin,
+                builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:8080")
+                    .AllowCredentials()
+                    .AllowAnyHeader();
+                });
+
+            });
+
             services.AddDbContext<DrinkStoreContext>(o =>
                 o.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
@@ -43,6 +57,8 @@ namespace DrinkStore.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(AllowedClientOrigin);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
