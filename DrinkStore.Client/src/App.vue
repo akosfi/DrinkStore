6443c754ff5418b63140bc4b9b67f4bfc2d16385
Ákos Fi <fiakos1997@gmail.com>
2020-03-31 10:41:54 +0200

    initial vue app

---
+++ DrinkStore.Client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        { mine }
+    </div>
+</template>
+
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    const App = Vue.extend({
+        data: function() {
+            return {
+                mine: 'mine',
+            };
+        }
+    });
+
+    export default App; 
+</script>
+
+<style lang="scss">
+
+</style>
